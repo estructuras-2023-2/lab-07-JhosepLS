179501da52736822249275a82cbd315512620370
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-11-03 01:11:43 -0500

    Implementamos la lógica del juego en student_file.h

--- student_file.h
+++ student_file.h
@@ -10,11 +10,36 @@ public:
 
 vector<int> Jugadores::Torneo(vector<int> habilidades, int N, int K) {
     queue<int> cola;
-    int actualGanador = habilidades[0];
-    int actualPerdedor = habilidades[1];
-    int contadorVictorias = 0;
-
+    
     for (int i = 2; i < habilidades.size(); ++i) {
         cola.push(habilidades[i]);
     }
+
+    int juegosGanados = 0;
+    int jugadorActual = habilidades[0];
+    int jugadorDesafiante = habilidades[1];
+
+    vector<int> resultado(2);
+    for (int juego = 1; juego <= K; ++juego) {
+        if (jugadorActual > jugadorDesafiante) {
+            juegosGanados++;
+            resultado = {jugadorDesafiante, jugadorActual};
+            cola.push(jugadorDesafiante);
+        } else {
+            juegosGanados = 1;
+            resultado = {jugadorActual, jugadorDesafiante};
+            cola.push(jugadorActual);
+            jugadorActual = jugadorDesafiante;
+        }
+        //Si el jugador gano n juegos, va al final
+        if (juegosGanados == N) {
+            cola.push(jugadorActual);
+            jugadorActual = cola.front();
+            cola.pop();
+            juegosGanados = 0;
+        }
+        jugadorDesafiante = cola.front();
+        cola.pop();
+    }
+    return resultado;
 }
