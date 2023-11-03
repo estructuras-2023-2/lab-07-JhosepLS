24811262664b0ebb30945155548c8ab45d73c931
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-11-03 01:14:51 -0500

    Añadimos todo lo necesario al main.cpp para que student_file.cpp funcione correctamente.
    Está con algunos pequeños cambios con respecto al código subido a gradescope debido a que no se como los test que se aplican al .h, entonces mi main.cpp lo aplica de otra forma, pero los cambios con el codigo .h aquí y el que está subido a gradescope son pequeños y no afectan en nada al código.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+#include "student_file.h"
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+    Jugadores j;
+
+    //Entrada 1
+    vector<int> habilidades1 = {1, 2, 3};
+    int N1 = 2, K1 = 2;
+    vector<int> resultado1 = j.Torneo(habilidades1, N1, K1);
+    cout << "{" << resultado1[0] << ", " << resultado1[1] << "}" << endl;
+
+    //Entrada 2
+    vector<int> habilidades2 = {49, 24, 26, 12, 5, 33, 25, 30, 35, 41, 46, 23, 21, 3, 38, 43, 11, 19, 34, 29, 20, 32, 39, 7, 50};
+    int N2 = 10, K2 = 399;
+    vector<int> resultado2 = j.Torneo(habilidades2, N2, K2);
+    cout << "{" << resultado2[0] << ", " << resultado2[1] << "}" << endl;
+
+    //Entrada 3
+    vector<int> habilidades3 = {1, 2, 3};
+    int N3 = 2, K3 = 4;
+    vector<int> resultado3 = j.Torneo(habilidades3, N3, K3);
+    cout << "{" << resultado3[0] << ", " << resultado3[1] << "}" << endl;
+
+    //Entrada 4
+    vector<int> habilidades4 = {30, 12};
+    int N4 = 34, K4 = 80;
+    vector<int> resultado4 = j.Torneo(habilidades4, N4, K4);
+    cout << "{" << resultado4[0] << ", " << resultado4[1] << "}" << endl;
+
+    return 0;
+}
