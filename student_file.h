9ff9bc1f8f93a1ac142481684dbbdc0e323ecf79
JhosepLS <143119700+JhosepLS@users.noreply.github.com>
2023-11-03 01:00:14 -0500

    Declarando las variables de control y estructura del método Torneo

--- student_file.h
+++ student_file.h
@@ -7,3 +7,14 @@ class Jugadores {
 public:
     vector<int> Torneo(vector<int> habilidades, int N, int K);
 };
+
+vector<int> Jugadores::Torneo(vector<int> habilidades, int N, int K) {
+    queue<int> cola;
+    int actualGanador = habilidades[0];
+    int actualPerdedor = habilidades[1];
+    int contadorVictorias = 0;
+
+    for (int i = 2; i < habilidades.size(); ++i) {
+        cola.push(habilidades[i]);
+    }
+}
